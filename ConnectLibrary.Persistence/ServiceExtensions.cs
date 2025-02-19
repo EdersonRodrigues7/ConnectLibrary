24969fca7119fd44ae82a00fc8ea36ff992b95ea
Ederson Rodrigues <ederson.rodrigues777@gmail.com>
2025-02-19 12:08:10 -0300

    feat(issue-1): Persistence - Configure dependency injection and sqlite db connection

---
+++ ConnectLibrary.Persistence/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+using ConnectLibrary.Domain.Interfaces;
+using ConnectLibrary.Persistence.Context;
+using ConnectLibrary.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConnectLibrary.Persistence;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Sqlite");
+
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+}
